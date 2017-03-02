f19eb970e56dacd4173a60ed3545343fbf07be04
Kilian Roberts <robertsk2@carleton.edu>
2017-03-01 20:15:45 -0600

    Refactored the code to make it more readable

--- src/sample/SetupWindow.java
+++ src/sample/SetupWindow.java
@@ -17,51 +17,28 @@ import javafx.stage.Stage;
 
 public class SetupWindow extends Application {
     Scene crsIDandNameScene;
-    BorderPane crsIDandNamePane;
 
     @Override
     public void start(Stage primaryStage) {
-        crsIDandNamePane = new BorderPane();
-
-        crsIDandNameScene = new Scene(crsIDandNamePane, 300, 250);
-        primaryStage.setScene(crsIDandNameScene);
         primaryStage.setTitle("Course Setup");
-
-        GridPane courseNameGrid = generateCourseNameGrid();
-
-
-        Button btnNext = new Button();
-        btnNext.setText("Next");
-        btnNext.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                    System.out.println("Works");
-            }
-        });
-
-        crsIDandNamePane.setCenter(courseNameGrid);
-        crsIDandNamePane.setBottom(btnNext);
-        crsIDandNamePane.setAlignment(btnNext, Pos.BOTTOM_RIGHT);
-        crsIDandNamePane.setMargin(btnNext, new Insets(0,15,15,0));
-
-
+        crsIDandNameScene = generateCrsIDandName();
+        primaryStage.setScene(crsIDandNameScene);
         primaryStage.show();
-
-
-
-
-
 
     }
 
-    private GridPane generateCourseNameGrid() {
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(15, 25, 25, 25));
-        grid.setHgap(10);
-        grid.setVgap(10);
+    private Scene generateCrsIDandName() {
+        BorderPane crsIDandNamePane = new BorderPane();
+        crsIDandNameScene = new Scene(crsIDandNamePane, 300, 200);
+
+        //------------------------------CREATE_INPUT_GRID---------------------------------------
+        GridPane inputGrid = new GridPane();
+        inputGrid.setPadding(new Insets(15, 25, 25, 25));
+        inputGrid.setHgap(10);
+        inputGrid.setVgap(10);
 
         Text gridTitle = new Text("Set up a new Course:");
-        grid.add(gridTitle, 0, 0);
+        inputGrid.add(gridTitle, 0, 0);
 
         Label identificationLabel = new Label("Course ID:");
         Label crsNameLabel = new Label("Course Name:");
@@ -69,13 +46,31 @@ public class SetupWindow extends Application {
         TextField identificationTextField = new TextField();
         TextField crsNameTextField = new TextField();
 
-        grid.add(identificationLabel, 0, 2);
-        grid.add(identificationTextField, 1, 2);
-        grid.add(crsNameLabel, 0, 3);
-        grid.add(crsNameTextField, 1, 3);
+        inputGrid.add(identificationLabel, 0, 2);
+        inputGrid.add(identificationTextField, 1, 2);
+        inputGrid.add(crsNameLabel, 0, 3);
+        inputGrid.add(crsNameTextField, 1, 3);
 
-        return grid;
-    }
+        crsIDandNamePane.setCenter(inputGrid);
+        //---------------------------------------------------------------------------------------
+
+        //------------------------------CREATE_NEXT_BUTTON---------------------------------------
+        Button btnNext = new Button();
+        btnNext.setText("Next");
+        btnNext.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Works");
+            }
+        });
+
+        crsIDandNamePane.setBottom(btnNext);
+        crsIDandNamePane.setAlignment(btnNext, Pos.BOTTOM_RIGHT);
+        crsIDandNamePane.setMargin(btnNext, new Insets(0, 15, 15, 0));
+        //---------------------------------------------------------------------------------------
+
+        return crsIDandNameScene;
+        }
 
     public static void main(String[] args) {launch(args);}
 }
