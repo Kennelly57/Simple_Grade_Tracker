427f0b8657b120d496257a160ded4ab768edcc63
michelsd <michelsd@cmc306-13.mathcs.carleton.edu>
2017-03-04 14:03:01 -0600

    expanding course overview. Making sample term and sample course.

---
+++ src/GradeTracker/CoursesOverviewDisplay.java
@@ -0,0 +1,122 @@
+package GradeTracker;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CoursesOverviewDisplay extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        start(primaryStage, "WI2017");
+    }
+
+    public void start(Stage primaryStage, String termName) {
+
+        BorderPane overviewBorderPane = new BorderPane();
+        overviewBorderPane.setPadding(new Insets(15, 15, 15, 25));
+        Text setupTitle = new Text("Your courses for ".concat(termName).concat(":"));
+        overviewBorderPane.setTop(setupTitle);
+        overviewBorderPane.setAlignment(setupTitle, Pos.CENTER);
+
+        GridPane dataPane = generateAssignmentCreationPane();
+        overviewBorderPane.setCenter(dataPane);
+
+        //------------------------------CREATE_FINALIZE_BUTTON-----------------------------------
+        Button btnFinish = new Button();
+        btnFinish.setText("+");
+        btnFinish.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Adding");
+            }
+        });
+
+        overviewBorderPane.setBottom(btnFinish);
+        overviewBorderPane.setAlignment(btnFinish, Pos.BOTTOM_RIGHT);
+        //---------------------------------------------------------------------------------------
+
+        Scene aswScene = new Scene(overviewBorderPane, 325, 350);
+        primaryStage.setScene(aswScene);
+        primaryStage.show();
+    }
+
+    public GridPane generateAssignmentCreationPane(){
+        GridPane dataGrid = new GridPane();
+        dataGrid.setHgap(10);
+        dataGrid.setVgap(10);
+        dataGrid.setPadding(new Insets(15, 0, 0, 0));
+
+        TextField categoryNameTF = new TextField();
+        TextField weightTF = new TextField();
+
+        HBox subItemHBox = generateSubItemHBox();
+
+        Label categoryNameLabel = new Label("Category Name:");
+        Label weightLabel = new Label("Weight:");
+        Label subItemsLabel = new Label("Subitems:");
+
+        VBox relevantFieldsVBox = generateRelevantFieldsVBox();
+
+        dataGrid.add(categoryNameLabel, 0, 0);
+        dataGrid.add(categoryNameTF, 1, 0);
+        dataGrid.add(weightLabel, 0, 1);
+        dataGrid.add(weightTF, 1, 1);
+        dataGrid.add(subItemsLabel, 0, 2);
+        dataGrid.add(subItemHBox, 1, 2);
+        dataGrid.add(relevantFieldsVBox, 0, 3);
+
+        return dataGrid;
+    }
+
+    private HBox generateSubItemHBox() {
+        ToggleGroup subItemsToggle = new ToggleGroup();
+        RadioButton btnSubItemsYes = new RadioButton("Yes");
+        RadioButton btnSubItemsNo = new RadioButton("No");
+        btnSubItemsYes.setToggleGroup(subItemsToggle);
+        btnSubItemsNo.setToggleGroup(subItemsToggle);
+
+        HBox subItemsHbox = new HBox();
+        subItemsHbox.setMargin(btnSubItemsYes, new Insets(0, 10, 0, 0));
+        subItemsHbox.getChildren().add(btnSubItemsYes);
+        subItemsHbox.getChildren().add(btnSubItemsNo);
+
+        return subItemsHbox;
+    }
+
+    private VBox generateRelevantFieldsVBox(){
+        VBox relevantFieldsVBox = new VBox();
+        Text relFieldsLabel = new Text("Relevant Fields");
+        relevantFieldsVBox.getChildren().add(relFieldsLabel);
+        relevantFieldsVBox.setMargin(relFieldsLabel, new Insets(10,0,10,0));
+
+        CheckBox pointsPossibleCB = new CheckBox("Points Possible");
+        CheckBox scorePointsCB = new CheckBox("Score (points)");
+        CheckBox scorePercentCB = new CheckBox("Score (percent)");
+        CheckBox weightCB = new CheckBox("Weight");
+        CheckBox weightedScoreCB = new CheckBox("Weighted Score");
+
+        List<CheckBox> cbList = Arrays.asList(
+                pointsPossibleCB, scorePointsCB, scorePercentCB,
+                weightCB, weightedScoreCB);
+        for(int i = 0; i < cbList.size(); i++){
+            relevantFieldsVBox.getChildren().add(cbList.get(i));
+        }
+
+        return relevantFieldsVBox;
+    }
+
+    public static void main(String[] args) {launch(args);}
+}
---
+++ src/GradeTracker/SampleCourse.java
@@ -0,0 +1,18 @@
+package GradeTracker;
+
+/**
+ * Created by michelsd on 3/4/17.
+ */
+public class SampleCourse {
+    public SampleCourse(String id, String name, String grade) {
+        String courseId = id;
+        String courseName = name;
+        String courseGrade = grade;
+    }
+}
+
+/*
+    String courseId = "BIOL.362";
+    String courseName = "Owl Patterns and Colors";
+    String courseGrade = "C";
+*/
