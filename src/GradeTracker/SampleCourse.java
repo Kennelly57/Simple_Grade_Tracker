8f413299575ee1ccb65c56f99c067d2a38b49f40
michelsd <michelsd@cmc306-13.mathcs.carleton.edu>
2017-03-04 15:01:38 -0600

    added sample objects

--- src/GradeTracker/SampleCourse.java
+++ src/GradeTracker/SampleCourse.java
@@ -4,15 +4,25 @@ package GradeTracker;
  * Created by michelsd on 3/4/17.
  */
 public class SampleCourse {
-    public SampleCourse(String id, String name, String grade) {
-        String courseId = id;
-        String courseName = name;
-        String courseGrade = grade;
+    public String id;
+    public String name;
+    public String grade;
+
+    public SampleCourse(String courseId, String courseName, String courseGrade) {
+        this.id = courseId;
+        this.name = courseName;
+        this.grade = courseGrade;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getGrade() {
+        return this.grade;
     }
 }
-
-/*
-    String courseId = "BIOL.362";
-    String courseName = "Owl Patterns and Colors";
-    String courseGrade = "C";
-*/
