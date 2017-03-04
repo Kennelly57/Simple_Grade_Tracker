1bbc0b7b5fe6c413479b0945c171800f12501442
Kennelly57 <jwk1996@hotmail.com>
2017-03-03 21:35:10 -0600

    Tweaked main, added basic login window (with no functionality)

--- src/GradeTracker/Main.java
+++ src/GradeTracker/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        LoginView login = new LoginView();
+        Parent scene = login.displayLogin();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(scene, 600, 275));
         primaryStage.show();
     }
 
