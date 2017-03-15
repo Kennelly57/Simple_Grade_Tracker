f7547f890ee41bce97e2ebb6b598442685b3b50d
Kilian Roberts <robertsk2@carleton.edu>
2017-03-15 12:40:56 -0500

    Added javadoc comments to modelCourse and ArrayListSaveAndLoad

--- src/GradeTracker/ArrayListSaveAndLoad.java
+++ src/GradeTracker/ArrayListSaveAndLoad.java
@@ -10,16 +10,18 @@ import java.util.Map;
 
 /**
  * Created by Jack on 3/12/2017.
+ * @author Jack Kennelly
  */
-
-
 public class ArrayListSaveAndLoad {
+    private static String FILENAME = "courseList.txt";
 
-
-    /* Stores the data that the user inputs into the program as a txt file in the local directory. */
+    /**
+     * Stores the data that the user inputs into the program as a txt file in the local directory.
+     * @param data an array list of string that will be stored in courseList.txt
+     */
     public static void storeCourseList(ArrayList<String> data) {
         try {
-            FileOutputStream fileStream = new FileOutputStream("courseList.txt", false);
+            FileOutputStream fileStream = new FileOutputStream(FILENAME, false);
             ObjectOutputStream outputStream = new ObjectOutputStream(fileStream);
             outputStream.writeObject(data);
             outputStream.close();
@@ -30,13 +32,17 @@ public class ArrayListSaveAndLoad {
     }
 
 
-    /* Retrieves the saved file from the local directory. The program assumes it will have the same name every time,
-    * as saving is not handled by the user (beyond clicking a button). */
+    /**
+     * Retrieves the saved file from the local directory. The program assumes it will have the same name every time,
+     * as saving is not handled by the user (beyond clicking a button).
+     * @return The array list of string that the file contains
+     * @throws FileNotFoundException
+     */
     public static ArrayList<String> returnCourseList() throws FileNotFoundException{
         ArrayList<String> sum = new ArrayList<String>();
         boolean temp = true;
         try {
-            FileInputStream fileStream = new FileInputStream("courseList.txt");
+            FileInputStream fileStream = new FileInputStream(FILENAME);
             ObjectInputStream inputStream = new ObjectInputStream(fileStream);
             try {
                 sum = (ArrayList<String>) inputStream.readObject();
